a7f846ae59c3e3579fdf26a397e813f5411002df
Jay Bittner <jtbitt@gmail.com>
2020-06-08 20:52:25 -0700

    Done tweaking login for now

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,5 +1,5 @@
-import { Image, TouchableOpacity, StyleSheet, View, Text } from 'react-native';
 import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
 
 import LoginButton from './LoginButton';
 
@@ -7,10 +7,10 @@ export default function Login() {
   return (
     <View style={styles.loginContainer}>
       <Image style={styles.logo} source={require('../assets/logo.png')} />
-      <View>
-        <LoginButton text='Sign in with Google' textColor='#929292' buttonColor='white' />
-        <LoginButton text='Sign in with Facebook' textColor='white' buttonColor='#265288' />
-        <LoginButton text='Sign in with Email' textColor='white' buttonColor='#d91224' />
+      <View style={styles.buttonContainer}>
+        <LoginButton icon='logo-google' iconColor='black' text='Sign in with Google' textColor='#929292' buttonColor='white' />
+        <LoginButton icon='logo-facebook' iconColor='white' text='Sign in with Facebook' textColor='white' buttonColor='#265288' />
+        <LoginButton icon='md-mail' iconColor='white' text='Sign in with Email' textColor='white' buttonColor='#d91224' />
         <LoginButton text='Create Account' textColor='black' buttonColor='#ffefc9' />
       </View>
     </View>
@@ -21,6 +21,9 @@ const styles = StyleSheet.create({
   loginContainer: {
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  buttonContainer: {
+    width: 300,
   },
   logo: {
     width: 200,
--- frontend/components/LoginButton.js
+++ frontend/components/LoginButton.js
@@ -1,34 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function LoginButton({
+  icon,
+  iconColor,
   text,
   textColor,
   buttonColor
 }) {
   const buttonStyle = {
-    backgroundColor: buttonColor
+    backgroundColor: buttonColor,
+    paddingVertical: icon === undefined ? 15 : 10 
+  };
+  const iconStyle = {
+    color: iconColor,
+    marginRight: icon === undefined ? 70 : 30
   };
   const textStyle = {
     color: textColor
   };
 
+  console.log(icon);
+
   return (
     <TouchableOpacity style={[styles.button, buttonStyle]}>
-      <Text style={[styles.buttonText, textStyle]}>{text}</Text>
+      <Ionicons style={iconStyle} name={icon} size={30} color={iconColor} />
+      <Text style={[styles.buttonText, textStyle]}>
+        {text}
+      </Text>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   button: {
-    paddingVertical: 10,
-    paddingHorizontal: 50,
-    marginBottom: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    marginBottom: 15
+  },
+  icon: {
   },
   buttonText: {
     fontSize: 16,
-    textAlign: 'center',
     fontWeight: 'bold'
   },
 });
